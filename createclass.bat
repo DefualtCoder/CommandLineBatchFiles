d4a7826e1007c9e1257b6f74e090c05af050f73f
DefualtCoder <boricua21am@gmail.com>
2020-11-22 01:06:52 -0500

    Update createclass.bat
    Added Uppercase to Include guards of .h file

--- createclass.bat
+++ createclass.bat
@@ -9,7 +9,13 @@
 echo Enter Class Name: 
 set /P ClassName=
 IF EXIST %CD%\%ClassName%.cpp goto warnings
-IF NOT EXIST %CD%\%ClassName%.cpp goto createclass
+IF NOT EXIST %CD%\%ClassName%.cpp goto setUpper
+
+:setUpper
+set UpperCaseMacro=for /L %%n in (1 1 2) do if %%n==2 (for %%# in (A B C D E F G H I J K L M N O P Q R S T U V W X Y Z) do set "result=!result:%%#=%%#!") else setlocal enableDelayedExpansion ^& set result=
+%UpperCaseMacro%%ClassName%
+set UpperClassName=%result%
+goto createclass
 
 :createclass
 (
@@ -26,8 +32,8 @@ IF NOT EXIST %CD%\%ClassName%.cpp goto createclass
 IF EXIST "C:\Program Files\notepad++\notepad++.exe" (notepad++ %CD%\%ClassName%.cpp) ELSE (notepad %CD%\%ClassName%.cpp)
 
 (
-	echo ^#ifndef ^_^_%ClassName%^_H^_^_
-	echo ^#define ^_^_%ClassName%^_H^_^_
+	echo ^#ifndef ^_^_%UpperClassName%^_H^_^_
+	echo ^#define ^_^_%UpperClassName%^_H^_^_
 	echo.
 	echo class %ClassName% ^{
 	echo 	private^:
@@ -40,10 +46,10 @@ IF EXIST "C:\Program Files\notepad++\notepad++.exe" (notepad++ %CD%\%ClassName%.
 	echo.
 	echo ^}^;
 	echo.
-	echo ^#endif ^/^/ ^_^_%ClassName%^_H^_^_
+	echo ^#endif ^/^/ ^_^_%UpperClassName%^_H^_^_
 ) > %CD%\%ClassName%.h
 IF EXIST "C:\Program Files\notepad++\notepad++.exe" (notepad++ %CD%\%ClassName%.h) ELSE (notepad %CD%\%ClassName%.h)
-goto ss
+goto finished
 
 
 :warnings
@@ -52,7 +58,7 @@ echo Class "%ClassName%" already exists^! Enter different class name^!
 echo.
 goto makeclass
 
-:ss
+:finished
 echo.
 echo Class %ClassName% created^!
 echo.
